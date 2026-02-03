364920cb227355c68c36081cb8110d4db2410667
Yhaliff <whyis2cool@gmail.com>
2026-02-03 01:20:36 -0600

    Created GameTimer.

--- Scripts/game_timer.gd
+++ Scripts/game_timer.gd
@@ -1,9 +1,34 @@
-extends Node
+class_name GameTimer extends Node
 
-func _ready() -> void:
-	pass # Replace with function body.
+signal event_timing_end;
+
+var current_delta_time: float = 0.0
+
+var time_until_event: float = 1.0:
+	set(value):
+		time_until_event = clampf(value, 0.001, 9999999999999999999.0)
+
+var _is_timing: bool = false;
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if _is_timing == false: return
+	current_delta_time += delta
+	if current_delta_time >= time_until_event:
+		current_delta_time = 0.0
+		event_timing_end.emit()
+
 	pass
+
+
+func init() -> void:
+	current_delta_time = 0.0
+	_is_timing = false
+
+
+func start_timing() -> void:
+	_is_timing = true
+
+
+func end_timing() -> void:
+	_is_timing = false
